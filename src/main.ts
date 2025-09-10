0e3c29afd591b8cb6209dae2a8617a0eae529a20
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2025-09-10 20:00:25 +0900

    setup

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import "express-async-errors";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/api/hello", async (req, res) => {
+  res.json({ message: "Hello, express" });
+});
+
+app.use(errorHandler);
+
+app.get("/api/error", async (req, res) => {
+  throw new Error("Error Endpoint");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error("Unexpected error occurred", err);
+  res.status(500).send({
+    message: "Unexpected error occurred",
+  });
+}
