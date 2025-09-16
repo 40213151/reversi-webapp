20231a7d37f7434e2cd927b75c14641abccf87be
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2025-09-17 08:48:15 +0900

    盤面の初期化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@ import "express-async-errors";
 import morgan from "morgan";
 import mysql from "mysql2/promise";
 
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
+
+const INITIAL_BOARD = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+];
+
 const PORT = 3000;
 
 const app = express();
@@ -32,7 +47,42 @@ app.post("/api/games", async (req, res) => {
   try {
     await conn.beginTransaction();
 
-    await conn.execute("insert into games (started_at) values (?)", [now]);
+    const gameInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      "insert into games (started_at) values (?)",
+      [now]
+    );
+
+    const gameId = gameInsertResult[0].insertId;
+
+    const turnInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      "insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)",
+      [gameId, 0, DARK, now]
+    );
+
+    const turnId = turnInsertResult[0].insertId;
+    // 最初のマス目の数。マス目数の変更に対応できるように、ハードコードではなく、初期のマス目数から計算する。
+    const squareCount = INITIAL_BOARD.map((line) => line.length).reduce(
+      (v1, v2) => v1 + v2,
+      0
+    );
+
+    const squareInsertSql =
+      "insert into squares (turn_id, x, y, disc) values " +
+      Array.from(Array(squareCount))
+        .map(() => "(?, ?, ?, ?)")
+        .join(", ");
+
+    const squareInsertValues: any[] = [];
+    INITIAL_BOARD.forEach((line, y) => {
+      line.forEach((disc, x) => {
+        squareInsertValues.push(turnId);
+        squareInsertValues.push(x);
+        squareInsertValues.push(y);
+        squareInsertValues.push(disc);
+      });
+    });
+
+    await conn.execute(squareInsertSql, squareInsertValues);
 
     await conn.commit();
   } finally {
