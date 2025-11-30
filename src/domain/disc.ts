8cd3b55ea3b139e78121c706c4cb1e704ca4a675
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2025-11-30 17:55:25 +0900

    ドメインモデルのメソッドの簡易実装

--- src/domain/disc.ts
+++ src/domain/disc.ts
@@ -4,4 +4,8 @@ export const Disc = {
   LIGHT: 2,
 } as const;
 
-export type Disc = typeof Disc[keyof typeof Disc];
+export type Disc = (typeof Disc)[keyof typeof Disc];
+
+export function toDisc(value: number): Disc {
+  return value as Disc;
+}
