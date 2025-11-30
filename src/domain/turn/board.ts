08f83009ed6aa145bd4a2d5eb40c2af87b53059d
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2025-11-30 22:31:39 +0900

    盤面におけるかチェックする処理の実装

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,11 +1,27 @@
 import { Disc } from "../turn/disc";
 import { Move } from "../turn/move";
+import { Point } from "./point";
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
 
   place(move: Move): Board {
     // TODO 盤面におけるかチェック
+
+    // 空のマス目でない場合、置くことはできない
+    if (this._discs[move.point.y][move.point.x] !== Disc.EMPTY) {
+      throw new Error("selected point is not empty");
+    }
+
+    // ひっくり返せる点をリストアップ
+    const flipPoints = this.listFlipPoints();
+
+    // ひっくり返せる点がない場合、置くことはできない
+
+    if (flipPoints.length === 0) {
+      throw new Error("no flipped points");
+    }
+
     // 盤面をコピー
     const newDiscs = this._discs.map((line) => {
       return line.map((disc) => {
@@ -17,8 +33,11 @@ export class Board {
     newDiscs[move.point.y][move.point.x] = move.disc;
 
     // TODO ひっくり返す
-
     return new Board(newDiscs);
+  }
+
+  private listFlipPoints(): Point[] {
+    return [new Point(0, 0)];
   }
 
   get discs() {
